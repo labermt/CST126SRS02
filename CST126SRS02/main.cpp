35501fb9426c6ef9fd52db01a43eae2eeca8e0fd
Froenn <35356784+Froenn@users.noreply.github.com>
2018-01-25 21:02:45 -0800

    Created empty project
    -added main.cpp
    -created 'Solution Items' folder
    -added README.md to 'Solution Items'
    -added .gitignore to 'Solution Items'

---
+++ CST126SRS02/main.cpp
@@ -0,0 +1,18 @@
+/***********************************************************
+* Class ID:			Mespirit
+* Date Created:		1/25/18
+* Updated:			1/25/18
+* Assignment:		CST126SRS02
+* Filename:			main.cpp
+* Overview:
+* Issues:			None.
+* Notes:			char name [257];
+*					std::cin << setw(256) << name;
+*					check that nullptr != present in test cases
+***********************************************************/
+#include <iostream>
+
+int main()
+{
+	return 0;
+}

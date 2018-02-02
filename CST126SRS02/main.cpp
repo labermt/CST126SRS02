0cda4b70c5d34ee4270d2cd95247dddadaa22ec1
Froenn <35356784+Froenn@users.noreply.github.com>
2018-02-02 09:50:22 -0800

    Working on code

--- CST126SRS02/main.cpp
+++ CST126SRS02/main.cpp
@@ -1,20 +1,39 @@
 /***********************************************************
 * Class ID:			Mespirit
 * Date Created:		1/25/18
-* Updated:			1/25/18
+* Updated:			1/31/18
 * Assignment:		CST126SRS02
 * Filename:			main.cpp
 ***********************************************************/
 #include <iostream>
+#include <iomanip>
+#include <cstring>
 
-const int N_LEN[257];
+const int const name_len = 256;
 
 int main()
 {
+	char ** names = 0;
+
+	char name[name_len];
+
+	read(names, name);
+
+	populate();
+
 	return 0;
 }
+//pulls in line of text from user
+void read(char ** names, char name[]) 
+{
+	
+	std::cin >> std::setw(256) >> name;
+}
+//fill array with names
+void populate()
+{
 
-
+}
 //pull in lines of text (one line at a time) with max name length of 256 characters.
 
 //check for char or continuous chars, in the line of text. Also check for '\0' escape operator. 

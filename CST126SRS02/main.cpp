c651ff53c8fe0b0b4f979b1e30813d1ebf39ae4f
Janos Vamos <janos.vamos@oit.edu>
2018-02-03 10:23:34 -0800

    got arrays set up to copy and increase Having issues with deleting namesCopy

--- CST126SRS02/main.cpp
+++ CST126SRS02/main.cpp
@@ -13,32 +13,59 @@ int main()
 {
 	
 	char ** names = nullptr;
+	names = new char*[100];
+
+	char ** namesCopy = nullptr;
+	namesCopy = new char*[0];
+
 	char * nameRow = nullptr;
-	int numNames = 0;
-	char nameInput[257];
 	
-	names = new char*[20];
+	char nameInput[257];
+
+	char numNamesString[257];
+
+	int numNames = 0;
+		
+	bool sizeFlag = false;
+	
 	
 
 
 	for (int i = 0;;++i)
 	{
-		if (std::cin.peek() == '\n') { break; }  //Exits loop at end of line
-		
-		std::cin >> std::setw(256) >> nameInput;
-		size_t nameSize = strlen(nameInput) + 1;
+		if (std::cin.peek() == '\n' || std::cin.eof()) //Exits loop at end of line
+		{ 
+			break; 
+		}  
+		else // Increases number of rows in names array.
+		{
+			if (numNames > 0)
+			{
+				delete[] namesCopy;
+				namesCopy = new char*[i];
+				for (int x = 0; x < i; ++x)
+				{
+					namesCopy[x] = names[x];
+				}
 
+				delete[] names;
+				names = new char*[i + 1];
+
+				for (int x = 0; x < i; ++x)
+				{
+					names[x] = namesCopy[x];
+				}
+			}
+		}
+
+		std::cin >> std::setw(256) >> nameInput;
+		
+		
+		size_t nameSize = strlen(nameInput) + 1;
 		nameRow = new char[nameSize];
 		strcpy(nameRow, nameInput);
 		names[i] = nameRow;
 		
-		
-		//std::cout << std::endl;
-		//for (int j{}; j <= i; j++)
-		//{
-		//	
-		//	std::cout << names[j] << std::endl;
-		//}
 		++numNames;
 	}
 

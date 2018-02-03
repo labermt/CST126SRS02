ec2f6dcac980f41cbb4caf6a02f961ad7d42122c
wylder-gray <Wylder.gray@oit.edu>
2018-02-03 15:50:16 -0800

    Prints Last word First

--- CST126SRS02/CST126SRS02/CST126SRS02.cpp
+++ CST126SRS02/CST126SRS02/CST126SRS02.cpp
@@ -2,11 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstring>
 
 
 int main()
 {
-
+	char* walker{};
+	char* nameList;
+	nameList = new char[240];
+	char* spacer = nameList;
+	std::cin.get(nameList, 240);
+	while (*spacer != '\0') {
+		if (iswspace(*spacer)) {
+			walker = spacer;
+		};
+		++spacer;
+	};
+	*walker = '\0';
+	walker++;
+	std::cout << nameList;
+	std::cout << '\n';
+	std::cout << walker;
     return 0;
 }
 

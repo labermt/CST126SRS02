61246ec5208dd893f1cda03d63c7e913bd326864
Mitch Laber <labermt@gmail.com>
2018-02-27 09:00:26 -0800

    Graded.

--- CST126SRS02/main.cpp
+++ CST126SRS02/main.cpp
@@ -1,19 +1,24 @@
 #include "stdafx.h"
+
 #include <iostream>
 #include <iomanip>
 #include <cassert>
 
+// C++ statements are like sentences, use blank lines to separate paragraphs. 
+
 using namespace std;
+
 int main()
 {
-	auto capacity = 0;
+	auto capacity = 0U;
 	auto nameList = new char *[capacity];
-	auto size = 0U;//a literal unsigned 0, it cannot go to negative with U 
-	char name[258];
+	auto size = 0U; // a literal unsigned 0, it cannot go to negative with U 
+
 	while (cin.good())
 	{
-		auto nl = cin.peek(); //nl stands for newline 
-		if (nl == '\n' || nl == '\t')
+		const auto nl = cin.peek(); // nl stands for newline 
+
+		if (nl == '\n') // Tabs don't end the input. 
 		{
 			assert(size <= capacity);
 			while (size != 0)
@@ -24,11 +29,12 @@ int main()
 			}
 			cout << endl;
 		}
+
 		if (size == capacity)
 		{
 			capacity = 2 * capacity + 1;
 			auto const temp = new char*[capacity];
-			int i{};
+			unsigned i{}; // Should be std::size_t.
 			while (i < size)
 			{
 				temp[i] = nameList[i];
@@ -39,12 +45,15 @@ int main()
 		}
 		else
 		{
-			cin >> setw(sizeof(name) - 1) >> name;//this is a static compiler knows about it 
-			auto heapName = new char[strlen(name) + 1];
+			char name[258]; // Keep data as local as possible. 
+
+			cin >> setw(sizeof(name) - 1) >> name; // this is a static compiler knows about it 
+			const auto heapName = new char[strlen(name) + 1];
 			strcpy(heapName, name);
 			nameList[size] = heapName;
 			size++;
 		}
 	}
+
 	return 0;
 }

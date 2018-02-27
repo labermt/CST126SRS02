d73ff2e2577ccc8af85fdf18d526eab794250ed1
Mitch Laber <labermt@gmail.com>
2018-02-27 13:43:23 -0800

    Graded.

--- CST126SRS02/main.cpp
+++ CST126SRS02/main.cpp
@@ -1,74 +1,75 @@
-// CST126SRS02.cpp : Defines the entry point for the console application.
+// main.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 #include <iomanip>
-#include <fstream>
+//#include <fstream> // NO FILE I/O as per spec. 
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-
-	char ** nameList = nullptr;
-
 	int capacity{};
 	int size{}; 
 
-	nameList = new char*[capacity];
+	auto nameList = new const char*[capacity]; // zero allocation is technically okay, but it must be deleted. nullptr not so. 
 
-	cout << "Feed me strings, Seymour! \n";
-
-	//Do stuff until you reach the end of the file.
+	// Do stuff until you reach the end of the file.
 	while (cin.good())
 	{
-		const auto nl = cin.peek(); //checking ahead for newline feed
+		const auto nl = cin.peek(); // checking ahead for newline feed
+		if (nl == EOF)
+		{
+			break;
+		}
 
 		if (nl != '\n')
 		{
-			if(size == capacity)
+			if (isspace(nl))
 			{
-				//Add space and allocate to heap
-				capacity = (capacity * 2) + 1;
-				auto const temp = new char*[capacity];
-
-				for (int copy{}; copy < size; copy++) temp[copy] = nameList[copy];
-
-				delete[] nameList;
-				nameList = temp;
+				cin.ignore();
 			}
-			while (size < capacity && cin.peek() != '\n')
+			else
 			{
-				char * currentName = nullptr;
-				char buffer[258]; // should that be 257?
+				if (size == capacity)
+				{
+					// Add space and allocate to heap
+					capacity = (capacity * 2) + 1;
+					auto const temp = new const char*[capacity];
+
+					for (int copy{}; copy < size; copy++) temp[copy] = nameList[copy]; // Use memcpy() for faster code.
+
+					delete[] nameList;
+					nameList = temp;
+				}
+				char buffer[258]; // should that be 257? 258 is ideal, but you need to read the full amount and detect spec overrun. 
 
 				cin >> setw(sizeof(buffer) - 1) >> buffer;
 
-				currentName = new char[strlen(buffer) + 1];
+				const auto currentName = new char[strlen(buffer) + 1];
 
 				strcpy(currentName, buffer);
 
 				nameList[size] = currentName;
 				size++;
-				
 			}
 		}
 		else
 		{
+			cin.ignore(); // Moving this line for clarity. 
+
 			// Output data
-			while(size)
+			while (size)
 			{
 				cout << nameList[--size] << " ";
 				delete[] nameList[size];
 			}
-			cin.ignore();
 			cout << endl;
-			cout << "Feed me strings, Seymour! \n";
 		}
 	}
 
 	delete[] nameList;
+
     return 0;
 }
-
